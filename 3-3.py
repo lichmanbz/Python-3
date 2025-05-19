635a96e2a7efe4eca93ecfba6e4788be2c98f7fd
lichmanbz <Andrey_lichman@list.ru>
2025-05-19 15:20:51 +0300

    Update 3-3.py

--- 3-3.py
+++ 3-3.py
@@ -1,24 +1,9 @@
 from typing import Any, Dict, List, Union
 
 def function_name(search: str, status: bool, *args: Union[int, str], **kwargs: Dict[str, Any]) -> Union[List[int], str]:
-    """
-    Обрабатывает аргументы в зависимости от параметров search и status.
 
-    Параметры:
-    search (str): Определяет, какая часть аргументов будет обработана ('args' или 'kwargs').
-    status (bool): Определяет, как обрабатываются аргументы (истинное значение для фильтрации целых чисел).
-    *args (Union[int, str]): Неименованные аргументы, которые могут быть либо целыми числами, либо строками.
-    **kwargs (Dict[str, Any]): Именованные аргументы в виде пар "ключ-значение".
-
-    Возвращает:
     Union[List[int], str]: 
-        - Если search == "args" и status == True: Возвращает список целых чисел из args.
-        - Если search == "args" и status == False: Возвращает строку, составленную из args.
-        - Если search == "kwargs": Возвращает строку с ключами и значениями из kwargs.
-    
-    Исключения:
-    ValueError: Если значение search не равно 'args' или 'kwargs'.
-    """
+        - Если search 
     
     result: List[int] = []
     result_2: str = ""
